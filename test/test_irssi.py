00f94ccb6b6d90608d277fcc1421d03f4cfcc3aa
Per Mortensen <per@permortensen.com>
2017-11-23 16:33:08 +0100

    Use pytest instead of unittest

--- test/test_irssi.py
+++ test/test_irssi.py
@@ -1,76 +1,74 @@
-import unittest
 import io
 from readers.irssi import IrssiLogReader
 from models import QuoteType
 
-class TestIrssiLogReader(unittest.TestCase):
-    def test_utc_offset(self):
-        cases = [
-            (0, 20),
-            (1, 19),
-            (11, 9),
-            (-1, 21),
-            (-11, 7)
-        ]
-        for (offset_hours, expected_utc_hour) in cases:
-            lines = io.StringIO('20:56 <&Cassie> what the fuck')
-            reader = IrssiLogReader('', 0, offset_hours, '')
-            quote = next(reader.read(lines))
-            self.assertEqual(quote.timestamp.hour, expected_utc_hour)
-
-    def test_nick(self):
-        cases = ['Duo', 'You', 'you']
-        for nick in cases:
-            lines = io.StringIO('20:56 -!- %s is now known as udo' % nick)
-            reader = IrssiLogReader('', 0, 0, 'Skanker')
-            quote = next(reader.read(lines))
-            self.assertEqual(quote.quote_type, QuoteType.nick)
-            self.assertEqual(quote.author, nick)
-            self.assertEqual(quote.message, 'udo')
-
-    def test_you_nick(self):
-        lines = io.StringIO('20:56 -!- You\'re now known as udo')
-        reader = IrssiLogReader('', 0, 0, 'Duo')
-        quote = next(reader.read(lines))
-        self.assertEqual(quote.quote_type, QuoteType.nick)
-        self.assertEqual(quote.author, 'Duo')
-        self.assertEqual(quote.message, 'udo')
-
-    def test_ban(self):
-        cases = [
-            ('+b anyname!*@*', 'anyname'),
-            ('-o+b noskillbassist *!*@*.hsd1.ca.comcast.net', 'noskillbassist'),
-            ('-o+b Calum *!*moo@*.34329884.17AA9C3B.IP', 'Calum'),
-            ('+ic-S+b +v!*@*', '+v')
-        ]
-        for (mode, author) in cases:
-            lines = io.StringIO('20:56 -!- mode/#garachat [%s] by Ebichu' % mode)
-            reader = IrssiLogReader('', 0, 0, '')
-            quote = next(reader.read(lines))
-            self.assertEqual(quote.quote_type, QuoteType.ban)
-            self.assertEqual(quote.author, author)
-
-    def test_message(self):
+def test_utc_offset():
+    cases = [
+        (0, 20),
+        (1, 19),
+        (11, 9),
+        (-1, 21),
+        (-11, 7)
+    ]
+    for (offset_hours, expected_utc_hour) in cases:
         lines = io.StringIO('20:56 <&Cassie> what the fuck')
+        reader = IrssiLogReader('', 0, offset_hours, '')
+        quote = next(reader.read(lines))
+        assert quote.timestamp.hour == expected_utc_hour
+
+def test_nick():
+    cases = ['Duo', 'You', 'you']
+    for nick in cases:
+        lines = io.StringIO('20:56 -!- %s is now known as udo' % nick)
+        reader = IrssiLogReader('', 0, 0, 'Skanker')
+        quote = next(reader.read(lines))
+        assert quote.quote_type == QuoteType.nick
+        assert quote.author == nick
+        assert quote.message == 'udo'
+
+def test_you_nick():
+    lines = io.StringIO('20:56 -!- You\'re now known as udo')
+    reader = IrssiLogReader('', 0, 0, 'Duo')
+    quote = next(reader.read(lines))
+    assert quote.quote_type == QuoteType.nick
+    assert quote.author == 'Duo'
+    assert quote.message == 'udo'
+
+def test_ban():
+    cases = [
+        ('+b anyname!*@*', 'anyname'),
+        ('-o+b noskillbassist *!*@*.hsd1.ca.comcast.net', 'noskillbassist'),
+        ('-o+b Calum *!*moo@*.34329884.17AA9C3B.IP', 'Calum'),
+        ('+ic-S+b +v!*@*', '+v')
+    ]
+    for (mode, author) in cases:
+        lines = io.StringIO('20:56 -!- mode/#garachat [%s] by Ebichu' % mode)
         reader = IrssiLogReader('', 0, 0, '')
         quote = next(reader.read(lines))
-        self.assertEqual(quote.quote_type, QuoteType.message)
-        self.assertEqual(quote.author, '&Cassie')
-        self.assertEqual(quote.message, 'what the fuck')
+        assert quote.quote_type == QuoteType.ban
+        assert quote.author == author
 
-    def test_message_glitch(self):
-        lines = io.StringIO('20:56 20:56 <&Cassie> what the fuck<&Cassie> what the fuck')
-        reader = IrssiLogReader('', 0, 0, '')
-        quote = next(reader.read(lines))
-        self.assertEqual(quote.quote_type, QuoteType.message)
-        self.assertEqual(quote.author, '&Cassie')
-        self.assertEqual(quote.message, 'what the fuck')
+def test_message():
+    lines = io.StringIO('20:56 <&Cassie> what the fuck')
+    reader = IrssiLogReader('', 0, 0, '')
+    quote = next(reader.read(lines))
+    assert quote.quote_type == QuoteType.message
+    assert quote.author == '&Cassie'
+    assert quote.message == 'what the fuck'
 
-    def test_skip(self):
-        lines = io.StringIO('20:56 <&Cassie> what the fuck\n' +
-                            '20:56 -!- Duo is now known as udo\n' +
-                            '20:58 <&ashin> also swear words')
-        reader = IrssiLogReader('', 0, 0, '')
-        quotes = list(reader.read(lines, 2))
-        self.assertEqual(len(quotes), 1)
-        self.assertEqual(quotes[0].message, 'also swear words')
+def test_message_glitch():
+    lines = io.StringIO('20:56 20:56 <&Cassie> what the fuck<&Cassie> what the fuck')
+    reader = IrssiLogReader('', 0, 0, '')
+    quote = next(reader.read(lines))
+    assert quote.quote_type == QuoteType.message
+    assert quote.author == '&Cassie'
+    assert quote.message == 'what the fuck'
+
+def test_skip():
+    lines = io.StringIO('20:56 <&Cassie> what the fuck\n' +
+                        '20:56 -!- Duo is now known as udo\n' +
+                        '20:58 <&ashin> also swear words')
+    reader = IrssiLogReader('', 0, 0, '')
+    quotes = list(reader.read(lines, 2))
+    assert len(quotes) == 1
+    assert quotes[0].message == 'also swear words'
--- test/test_nda.py
+++ test/test_nda.py
@@ -1,46 +1,43 @@
-import unittest
 import io
 from readers.nda import NdaLogReader
 from models import QuoteType
 
-class TestNdaLogReader(unittest.TestCase):
-    def test_message(self):
-        lines = io.StringIO('2017-07-22 20:56:39.123456 :Cassie!~abc@sdf.dkf.com PRIVMSG #chan :what the fuck\r\n')
-        reader = NdaLogReader('#chan', 0, '')
-        quote = next(reader.read(lines))
-        self.assertEqual(quote.author, 'Cassie')
-        self.assertEqual(quote.message, 'what the fuck')
-        self.assertEqual(quote.quote_type, QuoteType.message)
-        self.assertEqual(quote.timestamp.year, 2017)
-        self.assertEqual(quote.timestamp.month, 7)
-        self.assertEqual(quote.timestamp.day, 22)
-        self.assertEqual(quote.timestamp.hour, 20)
-        self.assertEqual(quote.timestamp.minute, 56)
-        self.assertEqual(quote.timestamp.second, 39)
+def test_message():
+    lines = io.StringIO('2017-07-22 20:56:39.123456 :Cassie!~abc@sdf.dkf.com PRIVMSG #chan :what the fuck\r\n')
+    reader = NdaLogReader('#chan', 0, '')
+    quote = next(reader.read(lines))
+    assert quote.author == 'Cassie'
+    assert quote.message == 'what the fuck'
+    assert quote.quote_type == QuoteType.message
+    assert quote.timestamp.year == 2017
+    assert quote.timestamp.month == 7
+    assert quote.timestamp.day == 22
+    assert quote.timestamp.hour == 20
+    assert quote.timestamp.minute == 56
+    assert quote.timestamp.second == 39
 
-    def test_nda_message(self):
-        lines = io.StringIO('2016-02-03 00:02:46.188104 Sending ^^ Sonic the Hedgehog (@sonic_hedgehog): Happy Hedgehog Day! https://t.co/AHkbFg74oo to #chan')
-        reader = NdaLogReader('#chan', 0, 'nda')
-        quote = next(reader.read(lines))
-        self.assertEqual(quote.author, 'nda')
-        self.assertEqual(quote.message, '^^ Sonic the Hedgehog (@sonic_hedgehog): Happy Hedgehog Day! https://t.co/AHkbFg74oo')
-        self.assertEqual(quote.quote_type, QuoteType.message)
-        self.assertEqual(quote.timestamp.year, 2016)
-        self.assertEqual(quote.timestamp.month, 2)
-        self.assertEqual(quote.timestamp.day, 3)
-        self.assertEqual(quote.timestamp.hour, 0)
-        self.assertEqual(quote.timestamp.minute, 2)
-        self.assertEqual(quote.timestamp.second, 46)
+def test_nda_message():
+    lines = io.StringIO('2016-02-03 00:02:46.188104 Sending ^^ Sonic the Hedgehog (@sonic_hedgehog): Happy Hedgehog Day! https://t.co/AHkbFg74oo to #chan')
+    reader = NdaLogReader('#chan', 0, 'nda')
+    quote = next(reader.read(lines))
+    assert quote.author == 'nda'
+    assert quote.message == '^^ Sonic the Hedgehog (@sonic_hedgehog): Happy Hedgehog Day! https://t.co/AHkbFg74oo'
+    assert quote.quote_type == QuoteType.message
+    assert quote.timestamp.year == 2016
+    assert quote.timestamp.month == 2
+    assert quote.timestamp.day == 3
+    assert quote.timestamp.hour == 0
+    assert quote.timestamp.minute == 2
+    assert quote.timestamp.second == 46
 
-    def test_message_for_different_channel(self):
-        lines = io.StringIO('2017-07-22 20:56:39.123456 :Cassie!~abc@sdf.dkf.com PRIVMSG #chan :what the fuck\r\n')
-        reader = NdaLogReader('#notchan', 0, '')
-        quotes = list(reader.read(lines))
-        self.assertFalse(quotes)
+def test_message_for_different_channel():
+    lines = io.StringIO('2017-07-22 20:56:39.123456 :Cassie!~abc@sdf.dkf.com PRIVMSG #chan :what the fuck\r\n')
+    reader = NdaLogReader('#notchan', 0, '')
+    quotes = list(reader.read(lines))
+    assert not quotes
 
-    def test_nda_message_for_different_channel(self):
-        lines = io.StringIO('2016-02-03 00:02:46.188104 Sending ^^ Sonic the Hedgehog (@sonic_hedgehog): Happy Hedgehog Day! https://t.co/AHkbFg74oo to #chan')
-        reader = NdaLogReader('#notchan', 0, '')
-        quotes = list(reader.read(lines))
-        self.assertFalse(quotes)
-
+def test_nda_message_for_different_channel():
+    lines = io.StringIO('2016-02-03 00:02:46.188104 Sending ^^ Sonic the Hedgehog (@sonic_hedgehog): Happy Hedgehog Day! https://t.co/AHkbFg74oo to #chan')
+    reader = NdaLogReader('#notchan', 0, '')
+    quotes = list(reader.read(lines))
+    assert not quotes
